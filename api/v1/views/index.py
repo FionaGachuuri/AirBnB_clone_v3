ce1885fedcd58a08c14a06c093ca680ec69fb419
FionaGachuuri <fionairuka@gmail.com>
2025-02-17 08:19:20 +0300

    views

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Index view for API status check.'''
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    '''Returns the status of the API.'''
+    return jsonify({"status": "OK"})
