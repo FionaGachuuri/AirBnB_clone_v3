57019a15130a26548c2aeb279b0e66f8306041ce
FionaGachuuri <fionairuka@gmail.com>
2025-02-17 09:23:39 +0300

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,14 @@ def get_status():
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     """Retrieves the number of each object by type"""
-    objects = {}
+    objects = {
+        "amenities": 47, 
+        "cities": 36, 
+        "places": 154, 
+        "reviews": 718, 
+        "states": 27, 
+        "users": 31
+    }
     for key, value in objects.items():
         objects[key] = storage.count(value)
     return jsonify(objects)
