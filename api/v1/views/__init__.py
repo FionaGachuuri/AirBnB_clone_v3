ca324d498a57b1400a2f5af38e31aeab208591c8
FionaGachuuri <fionairuka@gmail.com>
2025-02-19 03:09:23 +0300

    city

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 """ blueprint for the app ap -AirBnB"""
 
 #from api.v1.views.amenities import *
-#from api.v1.views.cities import *
+from api.v1.views.cities import *
 from api.v1.views.index import *
 #from api.v1.views.places_amenities import *
 #from api.v1.views.places import *
